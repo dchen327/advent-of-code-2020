a053d53e26efe6f7183408cf33182c50a9b672d4
David Chen <davidc3287@gmail.com>
2020-12-02 09:29:53 -0500

    finish 2.2

--- aoc2.py
+++ aoc2.py
@@ -1,3 +1,4 @@
+# part 1
 f = 'ao2.txt'
 num_valid = 0
 for line in open(f).read().splitlines():
@@ -6,4 +7,14 @@ for line in open(f).read().splitlines():
     low, high = map(int, count_range.split('-'))
     count = pw.count(letter)
     num_valid += low <= count <= high
+print(num_valid)
+
+# part 2
+num_valid = 0
+for line in open(f).read().splitlines():
+    policy, pw = line.split(': ')
+    count_range, letter = policy.split()
+    idx1, idx2 = map(int, count_range.split('-'))
+    num_valid += (pw[idx1 - 1] == letter) ^ (pw[idx2 - 1] == letter)
+
 print(num_valid)
