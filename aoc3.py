0cd0b50ef8d655f7758af93c297ed75cb702d731
David Chen <davidc3287@gmail.com>
2020-12-03 09:48:54 -0500

    finish aoc3

---
+++ aoc3.py
@@ -0,0 +1,23 @@
+from operator import mul
+from functools import reduce
+
+map_arr = open('ao3.txt').read().splitlines()
+# part 1
+t = 0
+for r in range(1, len(map_arr)):
+    c = (r * 3) % len(map_arr[0])
+    if map_arr[r][c] == '#':  # Tree
+        t += 1
+print(t)
+# part 2
+vals = []
+slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+for R, C in slopes:
+    t = 0
+    for r in range(R, len(map_arr), R):
+        c = (r * C // R) % len(map_arr[0])
+        if map_arr[r][c] == '#':  # Tree
+            t += 1
+    vals.append(t)
+
+print(reduce(mul, vals, 1))  # product of list
