d08075278a258d8df7e64d00fd0965040b4d2bbc
David Chen <davidc3287@gmail.com>
2020-12-04 11:18:38 -0500

    finish part 1

--- aoc4.py
+++ aoc4.py
@@ -2,4 +2,11 @@
 passports = open('aoc4.txt').read().split('\n\n')  # split by blank lines
 passports = [p.replace('\n', ' ')
              for p in passports]  # replace new lines in passports
-print(passports[0])
+req_keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+t = 0
+for passport in passports:
+    keys = [pair.split(':')[0] for pair in passport.split()]
+    if all(req_key in keys for req_key in req_keys):
+        t += 1
+
+print(t)
