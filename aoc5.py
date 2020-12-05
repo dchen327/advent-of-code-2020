218ec66533dc8851b17da591d10392bac8096506
David Chen <davidc3287@gmail.com>
2020-12-05 09:26:17 -0500

    finish part 2

--- aoc5.py
+++ aoc5.py
@@ -1,8 +1,20 @@
+seats = open('aoc5.txt').read().splitlines()
+ids = []
+# part 1
 max_id = 0
-for seat in open('aoc5.txt').read().splitlines():
+for seat in seats:
     # convert to binary
     seat = seat.replace('F', '0').replace(
         'B', '1').replace('L', '0').replace('R', '1')
-    max_id = max(max_id, int(seat, 2))
+    id_ = int(seat, 2)
+    max_id = max(max_id, id_)
+    ids.append(id_)
 
 print(max_id)
+
+# part 2
+ids.sort()
+for i in range(len(ids) - 1):
+    if ids[i + 1] - ids[i] == 2:
+        print(ids[i] + 1)
+        break
