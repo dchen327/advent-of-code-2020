78493b9341e13f3d1222b74e2cd5134b086795ae
David Chen <davidc3287@gmail.com>
2020-12-05 09:19:54 -0500

    faster aoc5

--- aoc5.py
+++ aoc5.py
@@ -1,7 +1,4 @@
 for seat in open('aoc5.txt').read().splitlines():
-    row, col = seat[:7], seat[7:]
-    row = row.replace('F', '0').replace('B', '1')
-    col = col.replace('L', '0').replace('R', '1')
-    row = int(row, 2)  # convert from binary to dec
-    col = int(col, 2)
-    print(row, col)
+    seat = seat.replace('F', '0').replace(
+        'B', '1').replace('L', '0').replace('R', '1')
+    print(int(seat, 2))
