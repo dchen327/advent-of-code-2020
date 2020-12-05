49f177fb60c89d3fde19181acc59168b6a5cdc2b
David Chen <davidc3287@gmail.com>
2020-12-05 09:22:01 -0500

    finish part 1

--- aoc5.py
+++ aoc5.py
@@ -1,4 +1,8 @@
+max_id = 0
 for seat in open('aoc5.txt').read().splitlines():
+    # convert to binary
     seat = seat.replace('F', '0').replace(
         'B', '1').replace('L', '0').replace('R', '1')
-    print(int(seat, 2))
+    max_id = max(max_id, int(seat, 2))
+
+print(max_id)
