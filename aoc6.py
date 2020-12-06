294a7e1392d95779b85c3371a5fa0f0fc85bbd50
David Chen <davidc3287@gmail.com>
2020-12-06 08:42:26 -0500

    finish 6.2

--- aoc6.py
+++ aoc6.py
@@ -1,6 +1,19 @@
+from collections import defaultdict
+
 groups = open('aoc6.txt').read().split('\n\n')
-groups = [g.replace('\n', '') for g in groups]
+# part 1
+groups_no_spaces = [g.replace('\n', '') for g in groups]
+t = sum(len(set(group)) for group in groups_no_spaces)
+print(t)
+
+# part 2
 t = 0
 for group in groups:
-    t += len(set(group))
+    ppl = group.split('\n')
+    num_answers = defaultdict(int)
+    for person in ppl:
+        for answer in person:
+            num_answers[answer] += 1
+    t += list(num_answers.values()).count(len(ppl))
+
 print(t)
