479d5cd49dcfaaadef51f2fbc8774edbb4fe977f
David Chen <davidc3287@gmail.com>
2020-12-07 10:07:42 -0500

    clean up dfs

--- aoc7.py
+++ aoc7.py
@@ -9,27 +9,25 @@ for rule in rules:
     rules_dict[bag] = contents
 
 
-t = 0
-for color in rules_dict:
- # remove any numbers from left, remove plural
+def search(color):
+    global contains_shiny_gold
+    # remove any numbers from left, remove plural
     contents = [c.lstrip(digits).lstrip().rstrip('s')
                 for c in rules_dict[color]]
+    # print(contents)
+    for new_color in contents:
+        if new_color == 'shiny gold bag':
+            contains_shiny_gold = True
+            break
+        elif new_color == 'no other bag':
+            break
+        else:
+            search(new_color)
+
+
+t = 0
+for color in rules_dict:
     contains_shiny_gold = False
-
-    def search(color):
-        global contains_shiny_gold
-        contents = [c.lstrip(digits).lstrip().rstrip('s')
-                    for c in rules_dict[color]]
-        # print(contents)
-        for new_color in contents:
-            if new_color == 'shiny gold bag':
-                contains_shiny_gold = True
-                break
-            elif new_color == 'no other bag':
-                break
-            else:
-                search(new_color)
-
     search(color)
     if contains_shiny_gold:
         t += 1
