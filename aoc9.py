7eac757c215c49c0488b47280e9faacfa64202ec
David Chen <davidc3287@gmail.com>
2020-12-09 18:51:04 -0500

    implement two pointers

--- aoc9.py
+++ aoc9.py
@@ -1,6 +1,8 @@
 nums = open('aoc9.txt').read().splitlines()
 nums = list(map(int, nums))
-preamble_len = 25
+# part 1
+invalid_num = None
+preamble_len = 5
 for start_idx in range(len(nums) - preamble_len):
     a = nums[start_idx:start_idx + preamble_len]
     next_num = nums[start_idx + preamble_len]
@@ -11,5 +13,19 @@ for start_idx in range(len(nums) - preamble_len):
                 valid = True
                 break
     if not valid:
-        print(next_num)
+        invalid_num = next_num
         break
+
+print(invalid_num)
+# part 2
+l = 0
+sum_ = 0
+for r in range(len(nums)):
+    sum_ += nums[r]
+    while r - l >= 2 and sum_ > invalid_num:
+        sum_ -= nums[l]
+        l += 1
+        if sum_ == invalid_num:
+            x = nums[l:r+1]
+            print(min(x) + max(x))
+            break
