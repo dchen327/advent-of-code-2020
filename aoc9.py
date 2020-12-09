b5f465e5dedb523dc8bc1b0b1e24da687c0e8304
David Chen <davidc3287@gmail.com>
2020-12-09 18:42:02 -0500

    finish part 1

---
+++ aoc9.py
@@ -0,0 +1,15 @@
+nums = open('aoc9.txt').read().splitlines()
+nums = list(map(int, nums))
+preamble_len = 5
+for start_idx in range(len(nums) - preamble_len):
+    a = nums[start_idx:start_idx + preamble_len]
+    next_num = nums[start_idx + preamble_len]
+    valid = False
+    for i in range(preamble_len):
+        for j in range(i + 1, preamble_len):
+            if a[i] + a[j] == next_num:
+                valid = True
+                break
+    if not valid:
+        print(next_num)
+        break
