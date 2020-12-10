8eafa808b2787966d8ed295e3a9f297191c4a3f7
David Chen <davidc3287@gmail.com>
2020-12-10 08:51:47 -0500

    finish part 1 and speedups

--- aoc10.py
+++ aoc10.py
@@ -7,15 +7,17 @@ for _ in range(len(adapters)):
     new_chains = []
     for chain, remaining in chains:
         prev_jolt = chain[-1]
-        for jolt in remaining:
-            if 1 <= jolt - prev_jolt <= 3:
-                new_chain = chain[:] + [jolt]
-                new_remaining = remaining[:]
-                new_remaining.remove(jolt)
-                new_chains.append((new_chain, new_remaining))
+        jolt = remaining[0]
+        if 1 <= jolt - prev_jolt <= 3:
+            new_chain = chain[:] + [jolt]
+            new_remaining = remaining[:]
+            new_remaining.remove(jolt)
+            new_chains.append((new_chain, new_remaining))
+        else:
+            break
         chains = new_chains
 final_chain = chains[0][0]
 diffs = [final_chain[i + 1] - final_chain[i]
          for i in range(len(final_chain) - 1)]
 diffs.append(3)
-print(diffs.count(1), diffs.count(3))
+print(diffs.count(1) * diffs.count(3))
