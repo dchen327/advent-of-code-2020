cdce7ae89cc176a89257fc1eb0793e22ba7694c1
David Chen <davidc3287@gmail.com>
2020-12-10 09:43:44 -0500

    dynamic programming solution for part 2

--- aoc10.py
+++ aoc10.py
@@ -2,6 +2,8 @@ adapters = open('aoc10.txt').read().splitlines()
 adapters = list(map(int, adapters))
 adapters.sort()
 
+# part 1
+
 chains = [([0], adapters[:])]
 for _ in range(len(adapters)):
     new_chains = []
@@ -21,3 +23,16 @@ diffs = [final_chain[i + 1] - final_chain[i]
          for i in range(len(final_chain) - 1)]
 diffs.append(3)
 print(diffs.count(1) * diffs.count(3))
+
+# part 2
+counts = [1] + [0] * max(adapters)  # 1 way to get to 0
+for i in range(len(counts)):
+    if i in adapters:
+        if i == 1:
+            counts[i] = 1
+        elif i == 2:
+            counts[i] = counts[i-1] + counts[i-2]
+        else:
+            counts[i] = counts[i-1] + counts[i-2] + counts[i-3]
+
+print(counts[-1])
