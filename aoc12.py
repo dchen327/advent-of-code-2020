773070e2b6a3dd8c424eb7cc13f43b3a3dcf6ae2
David Chen <davidc3287@gmail.com>
2020-12-12 09:08:44 -0500

    finish 12.2

--- aoc12.py
+++ aoc12.py
@@ -1,5 +1,5 @@
 instructions = open('aoc12.txt').read().splitlines()
-print(instructions)
+# part 1
 x, y = 0, 0
 facing = 'E'
 dir_to_pos = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
@@ -21,4 +21,28 @@ for instruction in instructions:
         new_idx = (curr_idx + shift) % 4
         facing = directions[new_idx]
 
-print(x, y)
+print(abs(x) + abs(y))
+
+# part 2
+x, y = 0, 0  # ship pos
+wx, wy = 10, 1  # waypoint pos
+
+dir_to_pos = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
+for instruction in instructions:
+    action, value = instruction[0], int(instruction[1:])
+    if action in 'NESW':
+        dx, dy = dir_to_pos[action]
+        wx += value * dx
+        wy += value * dy
+    elif action == 'F':
+        x += value * wx
+        y += value * wy
+    elif action in 'LR':
+        if action == 'L':
+            action = 'R'
+            value = 360 - value  # convert all to right, or clockwise
+        num_rotations = (value // 90) % 4  # num 90 deg rotations
+        for _ in range(num_rotations):
+            wx, wy = wy, -wx  # rotate 90 degrees
+
+print(abs(x) + abs(y))
