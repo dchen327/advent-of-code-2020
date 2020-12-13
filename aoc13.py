bfb1136d62ef846464d2eab5932affc129e41286
David Chen <davidc3287@gmail.com>
2020-12-13 11:56:02 -0500

    finish 13.2

--- aoc13.py
+++ aoc13.py
@@ -1,6 +1,6 @@
 from math import ceil
 
-
+# part 1
 lines = open('aoc13.txt').read().splitlines()
 t = int(lines[0])
 buses = list(map(int, lines[1].replace('x,', '').split(',')))
@@ -12,3 +12,10 @@ for bus_id in buses:
         earliest_id = bus_id
 
 print(earliest_id * min_wait)
+
+# part 2
+# Generate list of (a, b) pairs such that x is congruent to a mod b
+# I'm passing this list into some Chinese remainder theorem code I wrote a while back
+buses = lines[1].split(',')
+pairs = [(-i, int(bus_id)) for i, bus_id in enumerate(buses) if bus_id != 'x']
+print(pairs)
