9fea935e80033754c402e798ea331cc01d2a3fa8
David Chen <davidc3287@gmail.com>
2020-12-16 21:36:36 -0500

    more cleanup for part 2

--- aoc16.py
+++ aoc16.py
@@ -4,13 +4,12 @@ rules, ticket, nearby_tickets = lines[0].split(
     '\n'), lines[1].split('\n')[-1], lines[2].split('\n')[1:]
 ticket = list(map(int, ticket.split(',')))
 nearby_tickets = [list(map(int, t.split(','))) for t in nearby_tickets]
-rules_formatted = []
+
+rules_formatted = {}
 for rule in rules:
+    field = rule[:rule.index(':')]
     rule = rule[rule.index(':')+2:]
-    # p1, p2 = rule.split(' or ')
-    # rules_formatted.append(p1)
-    # rules_formatted.append(p2)
-    rules_formatted.append(rule)
+    rules_formatted[field] = rule
 
 
 def check_rule(rule, num):
@@ -21,7 +20,7 @@ def check_rule(rule, num):
 error_rate = 0
 for nearby_ticket in nearby_tickets:
     for num in nearby_ticket:
-        if not any(check_rule(rule, num) for rule in rules_formatted):
+        if not any(check_rule(rule, num) for rule in rules_formatted.values()):
             error_rate += num
 
 print(error_rate)
