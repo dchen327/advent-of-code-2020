41a2202bdaae2a3fab570c43f7da9587497b932f
David Chen <davidc3287@gmail.com>
2020-12-17 10:23:36 -0500

    add range bound speedups

--- aoc17.py
+++ aoc17.py
@@ -2,16 +2,18 @@ import numpy as np
 from itertools import product
 
 init_state = open('aoc17.txt').read().splitlines()
-grid_size = 15
+grid_size = 30
 offset = grid_size // 2 - len(init_state) // 2
 grid = np.zeros((grid_size, grid_size, grid_size))
 for r in range(len(init_state)):
     for c in range(len(init_state[0])):
         grid[offset, r + offset, c + offset] = init_state[r][c] == '#'
 
+max_radius = len(init_state) // 2 + 2
 for _ in range(6):
     print(_ + 1)
-    for r, c, h in product(range(grid_size), repeat=3):
+    changes = []
+    for r, c, h in product(range(grid_size // 2 - max_radius, grid_size // 2 + max_radius), repeat=3):
         num_active_neighbors = 0
         for i, j, k in product(range(-1, 2), repeat=3):
             # find neighbors
@@ -20,10 +22,12 @@ for _ in range(6):
             if all(0 <= pos < grid_size for pos in [r + i, c + j, h + k]):
                 num_active_neighbors += grid[r + i, c + j, h + k]
         if grid[r, c, h] and num_active_neighbors not in [2, 3]:
-            new_grid[r, c, h] = 0
-        if not grid[r, c, h] and num_active_neighbors == 3:
-            new_grid[r, c, h] = 1
-    grid = new_grid
+            changes.append((r, c, h))
+        elif not grid[r, c, h] and num_active_neighbors == 3:
+            changes.append((r, c, h))
+    for change in changes:
+        grid[change] = 1 - grid[change]
+    max_radius += 1
 
 print(int(np.sum(grid)))
 
@@ -33,11 +37,12 @@ for r in range(len(init_state)):
     for c in range(len(init_state[0])):
         grid[offset, offset, r + offset, c + offset] = init_state[r][c] == '#'
 
-quit()
+max_radius = len(init_state) // 2 + 2
 for _ in range(6):
     print(_ + 1)
+    changes = []
     new_grid = np.copy(grid)
-    for r, c, h, t in product(range(grid_size), repeat=4):
+    for r, c, h, t in product(range(grid_size // 2 - max_radius, grid_size // 2 + max_radius), repeat=4):
         num_active_neighbors = 0
         for i, j, k, l in product(range(-1, 2), repeat=4):
             # find neighbors
@@ -46,9 +51,11 @@ for _ in range(6):
             if all(0 <= pos < grid_size for pos in [r + i, c + j, h + k, t + l]):
                 num_active_neighbors += grid[r + i, c + j, h + k, t + l]
         if grid[r, c, h, t] and num_active_neighbors not in [2, 3]:
-            new_grid[r, c, h, t] = 0
+            changes.append((r, c, h, t))
         if not grid[r, c, h, t] and num_active_neighbors == 3:
-            new_grid[r, c, h, t] = 1
-    grid = new_grid
+            changes.append((r, c, h, t))
+    for change in changes:
+        grid[change] = 1 - grid[change]
+    max_radius += 1
 
 print(int(np.sum(grid)))
