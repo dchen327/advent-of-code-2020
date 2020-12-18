a168c5b56149784d2d5fd337e30e9340135dec7f
David Chen <davidc3287@gmail.com>
2020-12-18 12:25:15 -0500

    pretty inefficient 18.2

--- aoc18.py
+++ aoc18.py
@@ -29,3 +29,36 @@ for problem in hw:
     t += int(calc(problem))
 
 print(t)
+
+# part 2
+
+
+def calc2(expr):
+    """ calculate expression from left to right, add before mul """
+    if '(' in expr:
+        end = expr.index(')')
+        for i in range(end - 1, -1, -1):
+            if expr[i] == '(':
+                paren_expr = expr[i:end+1]
+                return calc2(expr.replace(paren_expr, calc2(paren_expr[1:-1])))
+    else:
+        vals = expr.replace('+', ' ').replace('*', ' ').split()
+        ops = [i for i in expr if i in '+*']
+        for i in range(len(ops) - 1, -1, -1):
+            if ops[i] == '+':
+                s = int(vals[i]) + int(vals[i + 1])
+                del vals[i + 1]
+                del ops[i]
+                vals[i] = str(s)
+        prod = 1
+        for i in vals:
+            prod *= int(i)
+        return str(prod)
+
+
+t = 0
+for problem in hw:
+    problem = problem.replace(' ', '')
+    t += int(calc2(problem))
+
+print(t)
